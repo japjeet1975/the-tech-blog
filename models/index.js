d07050ac648df6f31c3ee4fa0dd54dc09e9345fe
japjeet1975 <japjeet1975@gmail.com>
2021-11-24 14:11:54 -0500

    added the styling

--- models/index.js
+++ models/index.js
@@ -2,33 +2,34 @@ const User = require('./User');
 const Post = require('./Post');
 const Comment = require('./Comment');
 
-// create associations
-
-//  user and post
+// For users can make many posts 
 User.hasMany(Post, {
     foreignKey: 'user_id'
-});
+}); 
+
+// code for where a post can only belong to one user 
 Post.belongsTo(User, {
-    foreignKey: 'user_id',
-    onDelete: 'SET NULL'
+    foreignKey: 'user_id'
+})
+
+// code for a comment can only belong to one user 
+Comment.belongsTo(User, {
+    foreignKey: 'user_id'
 });
 
-//  user and comment
+// code for a comment can only belong to one user 
+Comment.belongsTo(Post, {
+    foreignKey: 'post_id'
+});
+
+// code for users who can make many comments 
 User.hasMany(Comment, {
     foreignKey: 'user_id'
 });
-Comment.belongsTo(User, {
-    foreignKey: 'user_id',
-    onDelete: 'SET NULL'
-});
 
-// post and comment
+// code for users who can make many posts 
 Post.hasMany(Comment, {
     foreignKey: 'post_id'
 });
-Comment.belongsTo(Post, {
-    foreignKey: 'post_id',
-    onDelete: 'SET NULL'
-});
 
-module.exports = { User, Post, Comment}
+module.exports = { User, Post, Comment };
